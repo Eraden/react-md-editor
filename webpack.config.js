8e44ff07a8ab7daf43c6aad307b8ed4af20f8abc
Adrian Wozniak <adrian.wozniak@ita-prog.pl>
2018-12-26 19:22:40 +0100

    Modernize

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ if (!process.env.NODE_ENV)
 const debug = process.env.NODE_ENV === "development";
 
 const plugins = [
-    new MiniCssExtractPlugin({ filename: "[name].[contenthash].css", chunkFilename: "[id].css" }),
+    new MiniCssExtractPlugin({ filename: "MarkdownEditor.css" }),
 ];
 
 module.exports = {
